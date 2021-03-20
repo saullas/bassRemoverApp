f4a9410a41e871597bb606e3ee37ed7594fd5d6b
saulasciante <ms1004@student.uni-lj.si>
2021-03-20 10:15:54 +0100

    loading icon, finetuning, added youtube frame

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_from_directory, jsonify
+from flask import Flask, render_template, request, send_from_directory, jsonify, after_this_request
 from werkzeug.utils import secure_filename
 import os
 import utils
@@ -16,40 +16,64 @@ utils.delete_all_audiofiles()
 utils.delete_uploads_timer()
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
+def homePage():
+    return render_template("upload_audio.html")
+
+
+@app.route("/uploadUrl", methods=["POST"])
+def upload_url():
+    return None
+
+
+@app.route("/uploadAudio", methods=["POST"])
 def upload_audio():
+    if request.files:
+        audiofile = request.files["audiofile"]
 
-    if request.method == "POST":
-        if request.files:
-            audiofile = request.files["audiofile"]
+        if not utils.allowed_file(audiofile.filename):
+            return jsonify({
+                "error": "Unsupported format.",
+                "status": 400
+            })
 
-            if not utils.allowed_file(audiofile.filename):
-                return render_template("upload_audio.html", message="Unsupported format")
+        try:
+            currtime = time.strftime("%Y%m%d-%H%M%S")
+            unique_filename = os.path.splitext(secure_filename(audiofile.filename))[0] + currtime + ".wav"
 
-            try:
-                currtime = time.strftime("%Y%m%d-%H%M%S")
-                unique_filename = os.path.splitext(secure_filename(audiofile.filename))[0] + currtime + ".wav"
+            audiofile_processed = remove_bass(audiofile)
+            audiofile_processed.export(app.config["PROCESSED_FOLDER"] + unique_filename, format="wav")
 
-                audiofile_processed = remove_bass(audiofile)
-                audiofile_processed.export(app.config["PROCESSED_FOLDER"] + unique_filename, format="wav")
-
-                return jsonify({
-                    "filename": unique_filename,
-                    "status": 200
-                })
-            except:
-                return jsonify({
-                    "error": "An error occured during processing. Please try again.",
-                    "status": 500
-                })
-
+            return jsonify({
+                "filename": unique_filename,
+                "status": 200
+            })
+        except:
+            return jsonify({
+                "error": "An error occured during processing. Please try again.",
+                "status": 500
+            })
     else:
-        return render_template("upload_audio.html")
+        return jsonify({
+            "error": "No files attached.",
+            "status": 400
+        })
 
 
 @app.route("/downloadAudio/<filename>", methods=["GET"])
 def download_audio(filename):
-    return send_from_directory(app.config["PROCESSED_FOLDER"], filename, as_attachment=True)
+    try:
+        @after_this_request
+        def remove_file(response):
+            os.remove(app.config["PROCESSED_FOLDER"] + filename)
+            return response
+
+        return send_from_directory(app.config["PROCESSED_FOLDER"], filename, as_attachment=True)
+    except:
+        return jsonify({
+            "error": "There was an error fetching your file.",
+            "status": 500
+        })
 
 
 if __name__ == '__main__':
